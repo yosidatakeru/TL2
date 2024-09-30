64e63da2d61c17087373d3129b6de4b9c7e0b455
ヨシダタケル <115062870+yosidatakeru@users.noreply.github.com>
2024-09-30 22:29:56 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include<cstdio>
 #include<cstdlib>
-int main()
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char* argv[10])
 {
-	printf("Hello,World\n");
+
+	//printf("Hello,World\n");
+	//勝手にウィンドが閉じるのを防止している
 	//system("pause");
 
+	for (int i = 0; i < argc; i++)
+	{
+		//文字列argvの1番を表示
+		printf(argv[i]);
+		//改行
+		printf("\n");
+	}
+	//勝手にウィンドが閉じるのを防止している
+	system("pause");
 	return 0;
 }
